76304283194d6a9a7289972339393427a8d92a98
XillionCode <xillion@napp9.com>
2026-08-10 06:59:37 -0400

    0.2.5 Add session helper stubs

--- src/sessions/create.ts
+++ src/sessions/create.ts
@@ -1,7 +1,16 @@
 import { query } from "../db/init.ts";
 import { insertSession } from "../db/inserts.ts";
 
-export async function createSession(user_id: number) { }
+export async function createSession(user_id: number): Promise<string> {
+  return "hello";
+}
+// Helper Functions
+export async function doesTokenExist(token: string): Promise<boolean> {
+  return true;
+}
+export async function doesUserIdExist(user_id: number): Promise<string> {
+  return "this would be empty for no, or contain the token if so";
+}
 
 export async function getNextSessionId(): Promise<number> {
   const result = await query<{ max_id: number }>(`
