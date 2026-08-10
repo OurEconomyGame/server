770d9cc5d7f8684c7e7d9f7ef2667a5824284943
XillionCode <xillion@napp9.com>
2026-08-10 06:49:43 -0400

    0.2.4 Add createSession stub

--- src/sessions/create.ts
+++ src/sessions/create.ts
@@ -1,6 +1,8 @@
 import { query } from "../db/init.ts";
 import { insertSession } from "../db/inserts.ts";
 
+export async function createSession(user_id: number) { }
+
 export async function getNextSessionId(): Promise<number> {
   const result = await query<{ max_id: number }>(`
 		?[max_id] := max(id), *session{id}
