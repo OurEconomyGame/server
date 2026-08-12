c121e90e098b0bfcbc9a027bb75b1fea18d834de
XillionCode <xillion@napp9.com>
2026-08-12 07:13:14 -0400

    0.9.0 Support user sorting and id in list

--- src/routing.ts
+++ src/routing.ts
@@ -29,7 +29,7 @@ export default async function route(request: Request): Promise<Response> {
 			return new Response(Bun.file("./openapi.json"));
 
 		case "/list/users":
-			return Response.json(await getAllUsersPublicInfo());
+			return Response.json(await getAllUsersPublicInfo(postParams));
 
 		case "/signup":
 			if (method !== "POST")
--- src/users/list.ts
+++ src/users/list.ts
@@ -1,15 +1,31 @@
 import { getAllUsers } from "../db/gets.ts";
-export async function getAllUsersPublicInfo() {
+interface PublicUser {
+  id: number;
+  username: string;
+  joined: number;
+  active: number;
+}
+export async function getAllUsersPublicInfo(params: Record<string, unknown>) {
+  const sortBy = params.sortBy;
   const users = await getAllUsers();
 
-  const publicInfo = [];
+  const publicInfo: PublicUser[] = [];
 
   for (const user of users) {
     publicInfo.push({
+      id: user.id,
       username: user.name,
       joined: user.created_at,
       active: user.last_accessed,
     });
+  }
+  switch (sortBy) {
+    case "name":
+      publicInfo.sort((a, b) => a.username.localeCompare(b.username));
+      break;
+    default:
+      publicInfo.sort((a, b) => a.id - b.id);
+      break;
   }
   return publicInfo;
 }
