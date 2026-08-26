4643b7c0e50d47f42c8a800411bb466b1423d914
XillionCode <xillion@napp9.com>
2026-08-26 11:50:07 -0400

    0.11.4 Authenticate user in foundCompany

--- src/companies/found.ts
+++ src/companies/found.ts
@@ -1,3 +1,7 @@
+import { getUserBySessionToken } from "../sessions/check";
+
 export async function foundCompany(params: unknown, auth_token: string | null) {
+  const token: string = (auth_token !== null) ? auth_token | "";
+  const user = getUserBySessionToken(token);
   return { status: "Unimplemented" };
 }
