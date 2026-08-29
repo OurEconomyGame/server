7c71bce0283935237700af5618f7316f200bf463
XillionCode <xillion@napp9.com>
2026-08-29 06:17:17 -0400

    0.14.8 Add base production recipes

--- src/companies/production/base-recipies.ts
+++ src/companies/production/base-recipies.ts
@@ -6,6 +6,40 @@ import { Resources } from "./resources.ts";
  */
 export const nullRecipe = new BaseRecipe("Null Recipe", {}, Resources.Food, 1);
 
+export const grainRecipe = new BaseRecipe(
+	"Manual Grain Farm",
+	{ [Resources.Water]: 300 },
+	Resources.Grain,
+	150,
+);
+
+export const waterPump = new BaseRecipe("Water Pump", {}, Resources.Water, 500);
+
+export const geothermalPlant = new BaseRecipe(
+	"Geothermal Power Plant",
+	{ [Resources.Water]: 100 },
+	Resources.Electricity,
+	200,
+);
+
+export const electricWaterPump = new BaseRecipe(
+	"Electric Water Pump",
+	{ [Resources.Electricity]: 200 },
+	Resources.Water,
+	3000,
+);
+
+export const prePackedFood = new BaseRecipe(
+	"Pre Packaged Food",
+	{
+		[Resources.Electricity]: 550,
+		[Resources.Water]: 1000,
+		[Resources.Grain]: 100,
+	},
+	Resources.Food,
+	25,
+);
+
 /**
  * Registry of base production recipes available in the economy.
  */
